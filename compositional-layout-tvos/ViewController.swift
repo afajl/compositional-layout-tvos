f48edf460fd784499bd99acb878819cae243c44a
Paul Diaconescu <paul.diaconescu@svt.se>
2020-04-22 14:18:14 +0200

    Add minimal example

--- compositional-layout-tvos/ViewController.swift
+++ compositional-layout-tvos/ViewController.swift
@@ -9,12 +9,88 @@
 import UIKit
 
 class ViewController: UIViewController {
+    lazy var collectionView: UICollectionView = {
+        return UICollectionView(frame: .zero, collectionViewLayout: layout)
+    }()
+    
+    var layout: UICollectionViewCompositionalLayout = {
+        let layout = UICollectionViewCompositionalLayout {
+            (_: Int, _: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0)))
+            
+            item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+
+            let containerGroup = NSCollectionLayoutGroup.horizontal(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.2),
+                                                   heightDimension: .fractionalHeight(0.4)),
+                subitems: [item]
+            )
+
+            let section = NSCollectionLayoutSection(group: containerGroup)
+
+            section.orthogonalScrollingBehavior = .continuousGroupLeadingBoundary
+
+            return section
+        }
+        return layout
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        collectionView.dataSource = self
+        collectionView.register(CollectionCell.self, forCellWithReuseIdentifier: "collectionCell")
+        
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
     }
-
-
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath)
+        cell.backgroundColor = .systemPink
+        return cell
+    }
+}
+
+class CollectionCell: UICollectionViewCell {
+
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.adjustsImageWhenAncestorFocused = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "Image")
+        return imageView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setupView() {
+        contentView.addSubview(imageView)
+        imageView.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+    }
+}
